ecc21c73b9304a5fd883e587ed9ded40b2cb9091
MacDeMarquez <ca.marqueze@duocuc.cl>
2024-03-11 22:59:27 -0300

    Boton

---
+++ src/componentes/Boton.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+function Boton(props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !='.') && (valor !='=');
+  }
+
+  return (
+      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+        {props.children}
+      </div>
+    );
+}
+
+export default Boton;
